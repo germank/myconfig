c85c1ade5a07df9e4f3fdfdae7b429f534bb0917
Germán Kruszewski <germank@gmail.com>
2024-02-25 21:44:00 +0100

    fixing overwriting .bashrc

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
-cat <<-'EOF' > ~/.bashrc
+cat <<-'EOF' >> ~/.bashrc
 BASE16_SHELL="$HOME/.config/base16-shell/"
 [ -n "$PS1" ] && \
     [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
